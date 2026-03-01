eeaae943ed50419de9b76295d694b1f9092e53b6
NathanDM <nathan.damie@gmail.com>
2026-03-01 10:35:20 +0100

    fix: entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ echo "Running database migrations..."
 cd /app/backend
 /app/node_modules/.bin/drizzle-kit migrate --config=drizzle.config.ts
 echo "Starting NestJS..."
-exec node /app/backend/dist/main.js
+exec node /app/backend/dist/src/main.js
